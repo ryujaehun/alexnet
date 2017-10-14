0b60460170c06ae7802c87faec9bb616fca204bb
jaehun <2013103902@khu.ac.kr>
2017-10-14 12:11:56 +0900

    modifity alexnet

--- codes/alexnet.py
+++ codes/alexnet.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+default_path=''
 '''
 conv2d(
     input,
@@ -13,76 +14,122 @@ conv2d(
 Must have strides[0] = strides[3] = 1. For the most common case of the same horizontal and vertices strides, strides = [1, stride, stride, 1].
 first and fourth element's mean https://stackoverflow.com/questions/34642595/tensorflow-strides-argument
 '''
+class AlexNet(object):
+    def __init__(self,input,keep_prob=0.5,num_classes=1000,skip_layer,weights_path='Default'):
+        self.input=input
+        self.skip_layer=skip_layer
+        if weights_path=='Default':
+            self.weights_path=default_path
+        else:
+            self.weights_path=weights_path
+        self.num_classes=num_classes
+    self.alexnet()
+    
+    #load pretrained weights
+    def load_weights(self, session):
+        weights_dict = np.load(self.weights_path, encoding = 'bytes').item()
 
-def alexnet(image):
-    '''
-    Building the alexnet
+        for op_name in weights_dict:
+             if op_name not in self.skip_layer:
+                 with tf.variable_scope(op_name, reuse = True):
+                     for data in weights_dict[op_name]:
+                         if len(data.shape) == 1:
+                             var = tf.get_variable('biases',trainable=False)
+                             session.run(var.assign(data))
+                         else:
+                            var = tf.get_variable('weights',trainable=False)
+                            session.run(var.assign(data))
+    def alexnet(image):
+        '''
+        Building the alexnet
 
-    Args:
-    Image Tensor: shape is [batch_size,width,height,channels]
+        Args:
+        Image Tensor: shape is [batch_size,width,height,channels]
 
-    Returns:
-    poo(l5: the last Tensor in the convolutional component of AlexNet.
-    '''
-    # first conv
-    # 구체적인 스펙은 논문참조
-    with tf.name_scope('conv1') as scope:
-        kernel=tf.Variable(tf.truncated_normal([11,11,3,96],dtype=tf.float32,
-        stddev=1e-1),name='weights')
-        conv=tf.nn.conv2d(image,kernel,[1,4,4,1],padding='SAME')
-        biases=tf.Variable(tf.constant(0.0,shape=[96],dtype=tf.float32),
-        trainable=True,name='biases')
-        bias=tf.nn.bias_add(conv,biases)
+        Returns:
+        poo(l5: the last Tensor in the convolutional component of AlexNet.
+        '''
+        # first conv
+        # 구체적인 스펙은 논문참조
+        with tf.name_scope('conv1') as scope:
+            kernel=tf.Variable(tf.truncated_normal([11,11,3,96],dtype=tf.float32,
+            stddev=1e-1),name='weights')
+            conv=tf.nn.conv2d(image,kernel,[1,4,4,1],padding='SAME')
+            biases=tf.Variable(tf.constant(0.0,shape=[96],dtype=tf.float32),
+            trainable=True,name='biases')
+            bias=tf.nn.bias_add(conv,biases)
 
-    #lrn1
-    with tf.name_scope('lrn1') as scope:
-            lrn1=tf.nn.local_response_normalization(conv1,alpha=1e-4,beta=0.75,depth_radius=5,bias=2.0)
+        #lrn1
+        with tf.name_scope('lrn1') as scope:
+                lrn1=tf.nn.local_response_normalization(conv1,alpha=1e-4,beta=0.75,depth_radius=5,bias=2.0)
 
-    # pool1
-    # https://stackoverflow.com/questions/37674306/what-is-the-difference-between-same-and-valid-padding-in-tf-nn-max-pool-of-t
-    pool1=tf.nn.max_pool(lrn1,ksize=[1,3,3,1],strides=[1,2,2,1],padding='VALID',name='pool1')
+        # pool1
+        # https://stackoverflow.com/questions/37674306/what-is-the-difference-between-same-and-valid-padding-in-tf-nn-max-pool-of-t
+        pool1=tf.nn.max_pool(lrn1,ksize=[1,3,3,1],strides=[1,2,2,1],padding='VALID',name='pool1')
 
-    #conv2
-    # 논문에서는 2개의 gpu를 사용 conv2에서부터 2개로 나뉘게 되어 48개 이나 한개의 conv2를 가질예정이므로 96으로 수정
-    with tf.name_scope('conv2') as scope:
-        kernel=tf.Variable(tf.truncated_normal([5,5,96,256],dtype=tf.float32,stddev=1e-1),name='weights')
-        # stride에 대해서 따로 언급이 없으나 역산시 1
-        conv=tf.nn.conv2d(pool1,kernel,[1,1,1,1],padding='SAME')
-        biases=tf.Variable(tf.constant(0.0,shape=[256],dtype=tf.float32),trainable=True,name='biases')
-        bias=tf.nn.bias_add(conv,biases)
-        conv2=tf.nn.relu(bias,name=scope)
+        #conv2
+        # 논문에서는 2개의 gpu를 사용 conv2에서부터 2개로 나뉘게 되어 48개 이나 한개의 conv2를 가질예정이므로 96으로 수정
+        with tf.name_scope('conv2') as scope:
+            kernel=tf.Variable(tf.truncated_normal([5,5,96,256],dtype=tf.float32,stddev=1e-1),name='weights')
+            # stride에 대해서 따로 언급이 없으나 역산시 1
+            conv=tf.nn.conv2d(pool1,kernel,[1,1,1,1],padding='SAME')
+            biases=tf.Variable(tf.constant(0.0,shape=[256],dtype=tf.float32),trainable=True,name='biases')
+            bias=tf.nn.bias_add(conv,biases)
+            conv2=tf.nn.relu(bias,name=scope)
 
-    #lrn2
-    with tf.name_scope('lrn2') as scope:
-        lrn2=tf.nn.local_response_normalization(conv2,alpha=1e-4,beta=0.75,depth_radius=5,bias=2.0)
+        #lrn2
+        with tf.name_scope('lrn2') as scope:
+            lrn2=tf.nn.local_response_normalization(conv2,alpha=1e-4,beta=0.75,depth_radius=5,bias=2.0)
 
-    #pool2
-    pool2=tf.nn.max_pool(lrn2,ksize=[1,3,3,1],strides=[1,2,2,1],padding='VALID',name='pool2')
+        #pool2
+        pool2=tf.nn.max_pool(lrn2,ksize=[1,3,3,1],strides=[1,2,2,1],padding='VALID',name='pool2')
 
-    # conv3
-    with tf.name_scope('conv3') as scope:
-        kernel=tf.Variable(tf.truncated_normal([3,3,256,384],dtype=tf.float32,stddev=1e-1),name='weights')
-        conv=tf.nn.conv2d(pool2,kernel,[1,1,1,1],padding='SAME')
-        biases=tf.Variable(tf.constant(0,0,shape=[384],dtype=tf.float32),trainable=True,name='biases')
-        bias=tf.nn.bias_add(conv,biases)
-        conv3=tf.nn.relu(bias,name=scope)
+        # conv3
+        with tf.name_scope('conv3') as scope:
+            kernel=tf.Variable(tf.truncated_normal([3,3,256,384],dtype=tf.float32,stddev=1e-1),name='weights')
+            conv=tf.nn.conv2d(pool2,kernel,[1,1,1,1],padding='SAME')
+            biases=tf.Variable(tf.constant(0,0,shape=[384],dtype=tf.float32),trainable=True,name='biases')
+            bias=tf.nn.bias_add(conv,biases)
+            conv3=tf.nn.relu(bias,name=scope)
 
-    #conv4
-    with tf.name_scope('conv4') as scope:
-        kernel=tf.Variable(tf.truncated_normal([3,3,384,384],dtype=tf.float32,stddev=0.1),name='weights')
-        conv=tf.nn.conv2d(conv3,kernel,[1,1,1,1],padding='SAME')
-        biases=tf.Variable(tf.constant(0.0,shape=[384],dtype=tf.float32),trainable=True,name='biases')
-        bias=tf.nn.bias_add(conv,biases)
-        conv4=tf.nn.relu(bias,name=scope)
+        #conv4
+        with tf.name_scope('conv4') as scope:
+            kernel=tf.Variable(tf.truncated_normal([3,3,384,384],dtype=tf.float32,stddev=0.1),name='weights')
+            conv=tf.nn.conv2d(conv3,kernel,[1,1,1,1],padding='SAME')
+            biases=tf.Variable(tf.constant(0.0,shape=[384],dtype=tf.float32),trainable=True,name='biases')
+            bias=tf.nn.bias_add(conv,biases)
+            conv4=tf.nn.relu(bias,name=scope)
 
-    # conv5
-    with tf.name_scope('conv5') as scope:
-        kernel=tf.Variable(tf.truncated_normal([3,3,384,256],dtype=tf.float32,stddev=1e-1),name='weights')
-        conv=tf.nn.conv2d(conv4,kernel,[1,1,1,1],padding='SAME')
-        biases=tf.Variable(tf.constant(0.0,shape=[256],dtype=tf.float32),trainable=True,name='biases')
-        bias=tf.nn.bias_add(conv,biases)
-        conv5=tf.nn.relu(bias,name=scope)
+        # conv5
+        with tf.name_scope('conv5') as scope:
+            kernel=tf.Variable(tf.truncated_normal([3,3,384,256],dtype=tf.float32,stddev=1e-1),name='weights')
+            conv=tf.nn.conv2d(conv4,kernel,[1,1,1,1],padding='SAME')
+            biases=tf.Variable(tf.constant(0.0,shape=[256],dtype=tf.float32),trainable=True,name='biases')
+            bias=tf.nn.bias_add(conv,biases)
+            conv5=tf.nn.relu(bias,name=scope)
 
-    #pool5
-    pool5=tf.nn.max_pool(conv5,ksize=[1,3,3,1],strides=[1,2,2,1],padding='VALID',name='pool5')
-    return pool5
+        #pool5
+        pool5=tf.nn.max_pool(conv5,ksize=[1,3,3,1],strides=[1,2,2,1],padding='VALID',name='pool5')
+
+        # fully-connected6
+
+        flattened=tf.reshape(pool5,[-1,6*6*256])
+        fc6=self.fc(input=flattened,num_in=6*6*256,num_out=4096,name='fc6',drop_ratio=1.0-self.keep_prob)
+
+        # fully-connected6
+        fc7=self.fc(input=fc6,num_in=6*6*256,num_out=4096,name='fc7',drop_ratio=1.0-self.keep_prob)
+
+        # fully-connected6
+        self.fc8=self.fc(input=fc7,num_in=6*6*256,num_out=self.num_classes,name='fc8',drop_ratio=1.0-self.keep_prob)
+
+    def fc(self, input,num_in,num_out,name,drop_ratio=0.5):
+        with tf.variable_scope(name) as scope:
+            weights = tf.get_variable('weights',shape = [num_in,num_out],trainable=True)
+            biases = tf.get_variable('biases',[num_out],trainable=True)
+            # Linear
+            act = tf.nn.xw_plus_b(input,weights,biases,name=scope.name)
+            relu = tf.nn.relu(act)
+            if drop_ratio == 0:
+                return relu
+            else:
+                return tf.nn.dropout(relu,1.0-drop_ratio)
---
+++ codes/util.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework.ops import convert_to_tensor
+
+
+VGG_MEAN = tf.constant([123.68, 116.779, 103.939], dtype=tf.float32)
+
+
+
+class ImageDataGenerator(object):
+    def __init__(self, images, labels, batch_size, num_classes, shuffle=True):
+
+        self.img_paths = images
+        self.labels = labels
+        self.num_classes = num_classes
+        self.data_size = len(self.labels)
+        self.pointer = 0
+
+        if shuffle:
+            self._shuffle_lists()
+
+        self.img_paths = convert_to_tensor(self.img_paths, dtype=dtypes.string)
+        self.labels = convert_to_tensor(self.labels, dtype=dtypes.int32)
+        data = tf.contrib.data.Dataset.from_tensor_slices((self.img_paths, self.labels))
+        data = data.map(self._parse_function_train, num_threads=8,
+                        output_buffer_size=100 * batch_size)
+
+        data = data.batch(batch_size)
+
+        self.data = data
+
+
+    def _shuffle_lists(self):
+        path = self.img_paths
+        labels = self.labels
+        permutation = np.random.permutation(self.data_size)
+        self.img_paths = []
+        self.labels = []
+        for i in permutation:
+            self.img_paths.append(path[i])
+            self.labels.append(labels[i])
+
+
+    def _parse_function_train(self, filename, label):
+        one_hot = tf.one_hot(label, self.num_classes)
+        img_string = tf.read_file(filename)
+        img_decoded = tf.image.decode_png(img_string, channels=3)
+        img_resized = tf.image.resize_images(img_decoded, [227, 227])
+        img_centered = tf.subtract(img_resized, VGG_MEAN)
+        img_bgr = img_centered[:, :, ::-1]
+        return img_bgr, one_hot
