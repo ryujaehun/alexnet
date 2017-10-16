bc77c40f424f2617965e23ccfcb574900388608d
jaehun <2013103902@khu.ac.kr>
2017-10-17 00:04:18 +0900

    modifty layer

--- codes/alexnet.py
+++ codes/alexnet.py
@@ -1,54 +1,61 @@
 import tensorflow as tf
+import util as tu
 
-default_path=''
+
+def alexnet(image):
 '''
-conv2d(
-    input,
-    filter,
-    strides,
-    padding,
-    use_cudnn_on_gpu=None,
-    data_format=None,
-    name=None
-)
-Must have strides[0] = strides[3] = 1. For the most common case of the same horizontal and vertices strides, strides = [1, stride, stride, 1].
-first and fourth element's mean https://stackoverflow.com/questions/34642595/tensorflow-strides-argument
+Building the alexnet
+
+Args:
+Image Tensor: shape is [batch_size,width,height,channels]
+
+Returns:
+pool5: the last Tensor in the convolutional component of AlexNet.
 '''
-class AlexNet(object):
-    def __init__(self,input,keep_prob=0.5,num_classes=1000,skip_layer,weights_path='Default'):
-        self.input=input
-        self.skip_layer=skip_layer
-        if weights_path=='Default':
-            self.weights_path=default_path
-        else:
-            self.weights_path=weights_path
-        self.num_classes=num_classes
-    self.alexnet()
-    
-    #load pretrained weights
-    def load_weights(self, session):
-        weights_dict = np.load(self.weights_path, encoding = 'bytes').item()
+with tf.name_scope('alexnet') as scope:
+    with tf.name_scope('conv1') as inner_scope:
+        wcnn1 = tu.weight([11, 11, 3, 96], name='cnn1')
+        bcnn1 = tu.bias(0.0, [96], name='cnn1')
+        conv1 = tf.add(tu.conv2d(x, wcnn1, stride=(4, 4), padding='SAME'), bcnn1)
+        #conv1 = tu.batch_norm(conv1)
+        conv1 = tu.relu(conv1)
+        norm1 = tu.lrn(conv1, depth_radius=5, bias=2.0, alpha=1e-04, beta=0.75)
+        pool1 = tu.max_pool2d(norm1, kernel=[1, 3, 3, 1], stride=[1, 2, 2, 1], padding='VALID')
 
-        for op_name in weights_dict:
-             if op_name not in self.skip_layer:
-                 with tf.variable_scope(op_name, reuse = True):
-                     for data in weights_dict[op_name]:
-                         if len(data.shape) == 1:
-                             var = tf.get_variable('biases',trainable=False)
-                             session.run(var.assign(data))
-                         else:
-                            var = tf.get_variable('weights',trainable=False)
-                            session.run(var.assign(data))
-    def alexnet(image):
-        '''
-        Building the alexnet
+    with tf.name_scope('conv2') as inner_scope:
+        wcnn2 = tu.weight([5, 5, 96, 256], name='wcnn2')
+        bcnn2 = tu.bias(1.0, [256], name='bcnn2')
+        conv2 = tf.add(tu.conv2d(pool1, wcnn2, stride=(1, 1), padding='SAME'), bcnn2)
+        #conv2 = tu.batch_norm(conv2)
+        conv2 = tu.relu(conv2)
+        norm2 = tu.lrn(conv2, depth_radius=5, bias=2.0, alpha=1e-04, beta=0.75)
+        pool2 = tu.max_pool2d(norm2, kernel=[1, 3, 3, 1], stride=[1, 2, 2, 1], padding='VALID')
 
-        Args:
-        Image Tensor: shape is [batch_size,width,height,channels]
+    with tf.name_scope('conv3') as inner_scope:
+        wcnn3 = tu.weight([3, 3, 256, 384], name='wcnn3')
+        bcnn3 = tu.bias(0.0, [384], name='bcnn3')
+        conv3 = tf.add(tu.conv2d(pool2, wcnn3, stride=(1, 1), padding='SAME'), bcnn3)
+        #conv3 = tu.batch_norm(conv3)
+        conv3 = tu.relu(conv3)
 
-        Returns:
-        poo(l5: the last Tensor in the convolutional component of AlexNet.
-        '''
+    with tf.name_scope('alexnet_cnn_conv4') as inner_scope:
+        wcnn4 = tu.weight([3, 3, 384, 384], name='wcnn4')
+        bcnn4 = tu.bias(0.0, [384], name='bcnn4')
+        conv4 = tf.add(tu.conv2d(conv3, wcnn4, stride=(1, 1), padding='SAME'), bcnn4)
+        #conv4 = tu.batch_norm(conv4)
+        conv4 = tu.relu(conv4)
+
+    with tf.name_scope('alexnet_cnn_conv5') as inner_scope:
+        wcnn5 = tu.weight([3, 3, 384, 256], name='wcnn5')
+        bcnn5 = tu.bias(0.0, [256], name='bcnn5')
+        conv5 = tf.add(tu.conv2d(conv4, wcnn5, stride=(1, 1), padding='SAME'), bcnn5)
+        #conv5 = tu.batch_norm(conv5)
+        conv5 = tu.relu(conv5)
+        pool5 = tu.max_pool2d(conv5, kernel=[1, 3, 3, 1], stride=[1, 2, 2, 1], padding='VALID')
+
+    return pool5
+
+'''
         # first conv
         # 구체적인 스펙은 논문참조
         with tf.name_scope('conv1') as scope:
@@ -133,3 +140,50 @@ class AlexNet(object):
                 return relu
             else:
                 return tf.nn.dropout(relu,1.0-drop_ratio)
+'''
+
+def classifier(x, dropout):
+	"""
+	AlexNet fully connected layers definition
+
+	Args:
+		x: tensor of shape [batch_size, width, height, channels]
+		dropout: probability of non dropping out units
+
+	Returns:
+		fc3: 1000 linear tensor taken just before applying the softmax operation
+			it is needed to feed it to tf.softmax_cross_entropy_with_logits()
+		softmax: 1000 linear tensor representing the output probabilities of the image to classify
+
+	"""
+
+	pool5 = alexnet(x)
+
+	dim = pool5.get_shape().as_list()
+	flat_dim = dim[1] * dim[2] * dim[3] # 6 * 6 * 256
+	flat = tf.reshape(pool5, [-1, flat_dim])
+
+	with tf.name_scope('alexnet_classifier') as scope:
+		with tf.name_scope('alexnet_classifier_fc1') as inner_scope:
+			wfc1 = tu.weight([flat_dim, 4096], name='wfc1')
+			bfc1 = tu.bias(0.0, [4096], name='bfc1')
+			fc1 = tf.add(tf.matmul(flat, wfc1), bfc1)
+			# fc1 = tu.batch_norm(fc1)
+			fc1 = tu.relu(fc1)
+			fc1 = tf.nn.dropout(fc1, dropout)
+
+		with tf.name_scope('alexnet_classifier_fc2') as inner_scope:
+			wfc2 = tu.weight([4096, 4096], name='wfc2')
+			bfc2 = tu.bias(0.0, [4096], name='bfc2')
+			fc2 = tf.add(tf.matmul(fc1, wfc2), bfc2)
+			#fc2 = tu.batch_norm(fc2)
+			fc2 = tu.relu(fc2)
+			fc2 = tf.nn.dropout(fc2, dropout)
+
+		with tf.name_scope('alexnet_classifier_output') as inner_scope:
+			wfc3 = tu.weight([4096, 1000], name='wfc3')
+			bfc3 = tu.bias(0.0, [1000], name='bfc3')
+			fc3 = tf.add(tf.matmul(fc2, wfc3), bfc3)
+			softmax = tf.nn.softmax(fc3)
+
+	return fc3, softmax
--- codes/util.py
+++ codes/util.py
@@ -1,52 +1,283 @@
+import os
+import random
 import tensorflow as tf
 import numpy as np
-from tensorflow.python.framework import dtypes
-from tensorflow.python.framework.ops import convert_to_tensor
+from scipy.io import loadmat
+from PIL import Image
+
+#https://github.com/dontfollowmeimcrazy/imagenet
+
+################ TensorFlow standard operations wrappers #####################
+def weight(shape, name):
+	initial = tf.truncated_normal(shape, stddev=0.01)
+	w = tf.Variable(initial, name=name)
+	tf.add_to_collection('weights', w)
+	return w
+
+def bias(value, shape, name):
+	initial = tf.constant(value, shape=shape)
+	return tf.Variable(initial, name=name)
+
+def conv2d(x, W, stride, padding):
+	return tf.nn.conv2d(x, W, strides=[1, stride[0], stride[1], 1], padding=padding)
+
+def max_pool2d(x, kernel, stride, padding):
+	return tf.nn.max_pool(x, ksize=kernel, strides=stride, padding=padding)
+
+def lrn(x, depth_radius, bias, alpha, beta):
+	return tf.nn.local_response_normalization(x, depth_radius, bias, alpha, beta)
+
+def relu(x):
+	return tf.nn.relu(x)
+
+def batch_norm(x):
+	epsilon = 1e-3
+	batch_mean, batch_var = tf.nn.moments(x, [0])
+	return tf.nn.batch_normalization(x, batch_mean, batch_var, None, None, epsilon)
 
 
-VGG_MEAN = tf.constant([123.68, 116.779, 103.939], dtype=tf.float32)
 
 
 
-class ImageDataGenerator(object):
-    def __init__(self, images, labels, batch_size, num_classes, shuffle=True):
+################ batch creation functions #####################
 
-        self.img_paths = images
-        self.labels = labels
-        self.num_classes = num_classes
-        self.data_size = len(self.labels)
-        self.pointer = 0
+def onehot(index):
+	""" It creates a one-hot vector with a 1.0 in
+		position represented by index
+	"""
+	onehot = np.zeros(1000)
+	onehot[index] = 1.0
+	return onehot
 
-        if shuffle:
-            self._shuffle_lists()
+def read_batch(batch_size, images_source, wnid_labels):
+	""" It returns a batch of single images (no data-augmentation)
+		ILSVRC 2012 training set folder should be srtuctured like this:
+		ILSVRC2012_img_train
+			|_n01440764
+			|_n01443537
+			|_n01484850
+			|_n01491361
+			|_ ...
 
-        self.img_paths = convert_to_tensor(self.img_paths, dtype=dtypes.string)
-        self.labels = convert_to_tensor(self.labels, dtype=dtypes.int32)
-        data = tf.contrib.data.Dataset.from_tensor_slices((self.img_paths, self.labels))
-        data = data.map(self._parse_function_train, num_threads=8,
-                        output_buffer_size=100 * batch_size)
+		Args:
+			batch_size: batch size
+			images_sources: path to ILSVRC 2012 training set folder
+			wnid_labels: list of ImageNet wnid lexicographically ordered
 
-        data = data.batch(batch_size)
+		Returns:
+			batch_images: a tensor (numpy array of images) of shape [batch_size, width, height, channels]
+			batch_labels: a tensor (numpy array of onehot vectors) of shape [batch_size, 1000]
+	"""
+	batch_images = []
+	batch_labels = []
 
-        self.data = data
+	for i in range(batch_size):
+	    class_index = random.randint(0, 999)
+		folder = wnid_labels[class_index]
+        #image 를 읽어 list에 넣는다
+		batch_images.append(read_image(os.path.join(images_source, folder)))
+        # labels을 onehot
+        batch_labels.append(onehot(class_index))
 
 
-    def _shuffle_lists(self):
-        path = self.img_paths
-        labels = self.labels
-        permutation = np.random.permutation(self.data_size)
-        self.img_paths = []
-        self.labels = []
-        for i in permutation:
-            self.img_paths.append(path[i])
-            self.labels.append(labels[i])
+	np.vstack(batch_images)
+	np.vstack(batch_labels)
+	return batch_images, batch_labels
+
+def read_image(images_folder):
+	""" It reads a single image file into a numpy array and preprocess it
+
+		Args:
+			images_folder: path where to random choose an image
+
+		Returns:
+			im_array: the numpy array of the image [width, height, channels]
+	"""
+
+	image_path = os.path.join(images_folder, random.choice(os.listdir(images_folder)))
+
+	# load and normalize image
+	im_array = preprocess_image(image_path)
 
 
-    def _parse_function_train(self, filename, label):
-        one_hot = tf.one_hot(label, self.num_classes)
-        img_string = tf.read_file(filename)
-        img_decoded = tf.image.decode_png(img_string, channels=3)
-        img_resized = tf.image.resize_images(img_decoded, [227, 227])
-        img_centered = tf.subtract(img_resized, VGG_MEAN)
-        img_bgr = img_centered[:, :, ::-1]
-        return img_bgr, one_hot
+	return im_array
+
+def preprocess_image(image_path):
+	""" It reads an image, it resize it to have the lowest dimesnion of 256px,
+		it randomly choose a 224x224 crop inside the resized image and normilize the numpy
+		array subtracting the ImageNet training set mean
+
+		Args:
+			images_path: path of the image
+
+		Returns:
+			cropped_im_array: the numpy array of the image normalized [width, height, channels]
+	"""
+	IMAGENET_MEAN = [123.68, 116.779, 103.939] # rgb format 일때 전처리 hyperparameter
+
+	img = Image.open(image_path).convert('RGB')
+
+	# resize of the image (setting lowest dimension to 256px)
+	if img.size[0] < img.size[1]:
+		h = int(float(256 * img.size[1]) / img.size[0])
+		img = img.resize((256, h), Image.ANTIALIAS)
+	else:
+		w = int(float(256 * img.size[0]) / img.size[1])
+		img = img.resize((w, 256), Image.ANTIALIAS)
+
+	# random 224x224 patch
+	x = random.randint(0, img.size[0] - 224)
+	y = random.randint(0, img.size[1] - 224)
+    # 이미지를 짜른다. (224 초과분에서 224를 뺴서 )
+	img_cropped = img.crop((x, y, x + 224, y + 224))
+
+	cropped_im_array = np.array(img_cropped, dtype=np.float32)
+
+	for i in range(3):
+		cropped_im_array[:,:,i] -= IMAGENET_MEAN[i]
+
+	return cropped_im_array
+
+
+
+def read_k_patches(image_path, k):
+	""" It reads k random crops from an image
+
+		Args:
+			images_path: path of the image
+			k: number of random crops to take
+
+		Returns:
+			patches: a tensor (numpy array of images) of shape [k, 224, 224, 3]
+
+        # 이미지에서 몇 개의 patch를 더 추출
+	"""
+	IMAGENET_MEAN = [123.68, 116.779, 103.939] # rgb format
+
+	img = Image.open(image_path).convert('RGB')
+
+	# resize of the image (setting largest border to 256px)
+	if img.size[0] < img.size[1]:
+		h = int(float(256 * img.size[1]) / img.size[0])
+		img = img.resize((256, h), Image.ANTIALIAS)
+	else:
+		w = int(float(256 * img.size[0]) / img.size[1])
+		img = img.resize((w, 256), Image.ANTIALIAS)
+
+	patches = []
+	for i in range(k):
+		# random 244x224 patch
+		x = random.randint(0, img.size[0] - 224)
+		y = random.randint(0, img.size[1] - 224)
+		img_cropped = img.crop((x, y, x + 224, y + 224))
+
+		cropped_im_array = np.array(img_cropped, dtype=np.float32)
+
+		for i in range(3):
+			cropped_im_array[:,:,i] -= IMAGENET_MEAN[i]
+
+		patches.append(cropped_im_array)
+
+	np.vstack(patches)
+	return patches
+
+
+def read_validation_batch(batch_size, validation_source, annotations):
+    """ reading a batch of validation images from the validation set,
+    	groundthruths label are inside an annotations file """
+	batch_images_val = []
+	batch_labels_val = []
+
+	images_val = sorted(os.listdir(validation_source))
+
+	# reading groundthruths labels
+	with open(annotations) as f:
+		gt_idxs = f.readlines()
+		gt_idxs = [(int(x.strip()) - 1) for x in gt_idxs]
+
+	for i in range(batch_size):
+		# random image choice
+		idx = random.randint(0, len(images_val) - 1)
+
+		image = images_val[idx]
+		batch_images_val.append(preprocess_image(os.path.join(validation_source, image)))
+		batch_labels_val.append(onehot(gt_idxs[idx]))
+
+	np.vstack(batch_images_val)
+	np.vstack(batch_labels_val)
+	return batch_images_val, batch_labels_val
+
+################ Other helper procedures #####################
+
+def load_imagenet_meta(meta_path):
+	""" It reads ImageNet metadata from ILSVRC 2012 dev tool file
+
+		Args:
+			meta_path: path to ImageNet metadata file
+
+		Returns:
+			wnids: list of ImageNet wnids labels (as strings)
+			words: list of words (as strings) referring to wnids labels and describing the classes
+
+	"""
+	metadata = loadmat(meta_path, struct_as_record=False)
+
+	# ['ILSVRC2012_ID', 'WNID', 'words', 'gloss', 'num_children', 'children', 'wordnet_height', 'num_train_images']
+	synsets = np.squeeze(metadata['synsets'])
+	ids = np.squeeze(np.array([s.ILSVRC2012_ID for s in synsets]))
+	wnids = np.squeeze(np.array([s.WNID for s in synsets]))
+	words = np.squeeze(np.array([s.words for s in synsets]))
+	return wnids, words
+
+def read_test_labels(annotations_path):
+	""" It reads groundthruth labels from ILSRVC 2012 annotations file
+
+		Args:
+			annotations_path: path to the annotations file
+
+		Returns:
+			gt_labels: a numpy vector of onehot labels
+	"""
+	gt_labels = []
+
+	# reading groundthruths labels from ilsvrc12 annotations file
+	with open(annotations_path) as f:
+		gt_idxs = f.readlines()
+		gt_idxs = [(int(x.strip()) - 1) for x in gt_idxs]
+
+	for gt in gt_idxs:
+		gt_labels.append(onehot(gt))
+
+	np.vstack(gt_labels)
+
+	return gt_labels
+
+def format_time(time):
+	""" It formats a datetime to print it
+
+		Args:
+			time: datetime
+
+		Returns:
+			a formatted string representing time
+	"""
+	m, s = divmod(time, 60)
+	h, m = divmod(m, 60)
+	d, h = divmod(h, 24)
+	return ('{:02d}d {:02d}h {:02d}m {:02d}s').format(int(d), int(h), int(m), int(s))
+
+def imagenet_size(im_source):
+	""" It calculates the number of examples in ImageNet training-set
+
+		Args:
+			im_source: path to ILSVRC 2012 training set folder
+
+		Returns:
+			n: the number of training examples
+
+	"""
+	n = 0
+	for d in os.listdir(im_source):
+		for f in os.listdir(os.path.join(im_source, d)):
+			n += 1
+	return n
